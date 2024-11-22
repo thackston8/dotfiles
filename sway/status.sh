11073301d9ac41e88d566f68197ce2374bd89061
Christian Thackston <christian@thackston.dev>
2024-11-21 22:57:02 -0800

    Remove mpc from status bar

--- sway/status.sh
+++ sway/status.sh
@@ -2,31 +2,18 @@
 
 # Function to output status
 output_status() {
-    song=$(mpc current)
-    if [ -z "$song" ]; then
-        song="Not Playing"
-    fi
     date_formatted=$(date "+%a %m/%d %I:%M %P")
-    echo "$song | $date_formatted"
+    echo "$date_formatted"
 }
 
 # Initialize last minute
 last_minute=$(date "+%M")
-
-# Start mpc idleloop as a coprocess
-coproc mpc_idle { mpc idleloop player; }
 
 # **Output status immediately**
 output_status
 
 # Main loop
 while true; do
-    # Check for mpc events with a timeout of 1 second
-    if read -t 1 -u ${mpc_idle[0]} mpc_event; then
-        # mpc event occurred (song changed)
-        output_status
-    fi
-
     # Check if the minute has changed to update the clock
     current_minute=$(date "+%M")
     if [ "$current_minute" != "$last_minute" ]; then
